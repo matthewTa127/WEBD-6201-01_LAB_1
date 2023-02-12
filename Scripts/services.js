738cfea0bbd01838c71e2257cf635a257f435054
fendie07 <104850385+fendie07@users.noreply.github.com>
2023-02-12 18:49:42 -0500

    fixed issues with services page

--- Scripts/services.js
+++ Scripts/services.js
@@ -48,32 +48,33 @@
     bottomNavBar.textContent = "@Copyright 2023"
     mainContent.appendChild(bottomNavBar)
 //switch "Products" to "Projects" in navbar
-    let products = (document.getElementById("products").textContent)
-        console.log(products)
-        switch (products) {
-            case "Products":
-                document.getElementById("products").innerHTML = `<i class="fa-solid fa-bag-shopping"></i>Projects`
-                break;
+    
+let products = (document.getElementById("products").textContent)
+    console.log(products)
+    switch (products) {
+        case "Products":
+            document.getElementById("products").innerHTML = `<i class="fa-solid fa-bag-shopping"></i>Projects`
+            break;
         }
      
 
         
 // creates human resources link and icon, appends the two and inserts in the navbar before the 5th element in the list       
-        let humanResourcesIcon = document.createElement("i")
-        humanResourcesIcon.setAttribute("class", "fa-solid fa-person")
-        
-        let navBar = document.getElementById("navbarNav")
+let humanResourcesIcon = document.createElement("i")
+humanResourcesIcon.setAttribute("class", "fa-solid fa-person")
+
+let navBar = document.getElementById("navbarNav")
 
 
-        let humanResourcesLink = document.createElement("a")
-        let humanResources = document.createElement("li")
-        humanResources.setAttribute("class", "nav-item")
-        humanResourcesLink.innerHTML = "<i class = fa-solid fa-person></i>Human Resources"
-        humanResourcesLink.setAttribute("href", "#")
-        humanResourcesLink.setAttribute("class", "nav-link")
-        humanResourcesLink.insertBefore(humanResourcesIcon, humanResourcesLink.children[0])
-        humanResources.appendChild(humanResourcesLink)
-        navBar.insertBefore(humanResources, navBar.children[4])
+let humanResourcesLink = document.createElement("a")
+let humanResources = document.createElement("li")
+humanResources.setAttribute("class", "nav-item")
+humanResourcesLink.innerHTML = "<i class = fa-solid fa-person></i>Human Resources"
+humanResourcesLink.setAttribute("href", "#")
+humanResourcesLink.setAttribute("class", "nav-link")
+humanResourcesLink.insertBefore(humanResourcesIcon, humanResourcesLink.children[0])
+humanResources.appendChild(humanResourcesLink)
+navBar.insertBefore(humanResources, navBar.children[4])
 
     
 
